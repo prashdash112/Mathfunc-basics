aaadbccc957f7fa78dd0996288dca9a6622343d5
Prashant singh <prashdash112@gmail.com>
2021-01-23 14:11:57 +0530

    Create linked list.py

---
+++ linked list.py
@@ -0,0 +1,27 @@
+class node: # defining class node for creating several nodes 
+    
+    def __init__(self,data): # Function to initialise the node object 
+        
+        self.data=data; 
+        self.next=None
+
+# Linked List class contains a Node object 
+class linkedlist:       # defining class linkedlist for assigning the head of linkedlist to a node 
+    
+    def __init__(self): # Function to initialize head 
+        
+        self.head=None
+        
+if __name__=='__main__':
+    
+    lst=linkedlist() #  linkedlist object creation 
+    
+    lst.head=node(1) 
+    
+    nxt2=node(10)
+    
+    nxt3=node(100)
+    
+    lst.head.next=nxt2 # defining relations between different pointers
+    
+    nxt2.next=nxt3
