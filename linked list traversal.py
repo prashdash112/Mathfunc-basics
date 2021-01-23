1615e285387dc4caba2418ee5af562c84e146c0f
Prashant singh <prashdash112@gmail.com>
2021-01-23 14:43:55 +0530

    Update linked list traversal.py

--- linked list traversal.py
+++ linked list traversal.py
@@ -1,17 +1,17 @@
 # linked list traversal
 
-class node:
+class node: # defining class node for creating several nodes 
     
-    def __init__(self,data):
+    def __init__(self,data): # Function to initialise the node object 
         self.data=data
         self.next=None
         
-class linkedlist:
+class linkedlist: # Linked List class contains a Node object 
     
-    def __init__(self):
+    def __init__(self): # Function to initialize head 
         self.head=None
         
-    def print_list(self):
+    def print_list(self): #method definition for traversal of elements 
         temp=self.head
         while(temp):
             print(temp.data)
@@ -19,10 +19,10 @@ class linkedlist:
 
 if __name__=='__main__':
 
-    lst=linkedlist()
+    lst=linkedlist() #  linkedlist object creation 
     lst.head=node(1)
     nxt2=node(10)
     nxt3=node(100)
-    lst.head.next=nxt2
+    lst.head.next=nxt2 # defining relation between different nodes
     nxt2.next=nxt3
-    lst.print_list()
+    lst.print_list() # prints the elements of the linked list
